7c71be27cec8360bb544464ef7abdad5381abeeb
ilaedna16 <115794672+ilaedna16@users.noreply.github.com>
2022-11-16 16:14:24 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2,tiplo;  
+    
+    printf ("digite o primeiro numero"); 
+    scanf ("%d",&n1); 
+    printf ("digite o segundo numero"); 
+    scanf ("%d",&n2); 
+    
+    tiplo=n1*n1;
+    tiplo=n2*3;
+    
+    
+    printf ("\no triplo eh:%d", tiplo); 
+    
+    return 0;
+}
